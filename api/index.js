fdacc55de666a988412d41295b88402432c80543
Nishant Makwana <nishantmakwanacreations@gmail.com>
2024-11-18 13:29:38 +0530

    ADDED BACKEND

--- api/index.js
+++ api/index.js
@@ -1,23 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 3000;
 
 const { getOllamaResponse } = require("./routes/chatbot");
 
 app.use(cors());
 app.use(express.json());
 
-app.post("/chat", (req, res) => {
+app.post("/api/chat", (req, res) => {
   const userMessage = req.body.message;
   if (!userMessage) {
     return res.status(400).json({ error: "Message is required" });
   }
-
   const response = getOllamaResponse(userMessage);
   res.json({ response });
 });
 
-app.listen(port, () => {
-  console.log(`Server is listening on http://localhost:${port}`);
-});
+module.exports = app;
